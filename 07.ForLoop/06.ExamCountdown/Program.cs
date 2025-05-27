0eb29d703dfe91db276a5d40905cbaee8be84421
Diyan Apostolov <diyan.apostolov@yahoo.com>
2025-05-27 21:25:44 +0300

    Add solutions - For Loop

---
+++ 07.ForLoop/06.ExamCountdown/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int daysBeforTheExam = int.Parse(Console.ReadLine());
+
+for (int i = daysBeforTheExam; i >= 1; i--)
+{
+    Console.WriteLine($"{i} days before the exam");
+}
+
+Console.WriteLine("The exam has come");
---
+++ 07.ForLoop/07.LatinLetters/Program.cs
@@ -0,0 +1,8 @@
+﻿
+char startLetter = char.Parse(Console.ReadLine());
+char endLetter = char.Parse(Console.ReadLine());
+
+for (char i = startLetter; i <= endLetter; i++)
+{
+    Console.Write(i + " ");
+}
